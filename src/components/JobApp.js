573e4c06629e5b65c85fc0f8fb140448acf25dce
knguye1999 <55815753+knguye1999@users.noreply.github.com>
2021-03-15 19:03:00 -0700

    Add files via upload

---
+++ src/components/JobApp.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './JobApp.css';
+
+export default function JobApp({
+  company, position, description, status, date,
+}) {
+  const [color] = useState(
+    status === 'Applied' ? '#c74e9f'
+      : status === 'Rejected' ? '#ff2e43'
+        : status === 'Interview' ? '#4466ee'
+          : status === 'Offer' ? '#88e234' : '#000'
+  );
+  return (
+    <li className="JobApp" style={{ backgroundColor: color }}>
+      <ul>
+        <li>
+          <b>Company:&nbsp;</b>
+          <span id="tabbed">{company}</span>
+        </li>
+        <li>
+          <b>Position:&nbsp;</b>
+          <span id="tabbed">{position}</span>
+        </li>
+        <li>
+          <b>Date:&nbsp;</b>
+          <span id="tabbed">{date}</span>
+        </li>
+        <li>
+          <b>Description:&nbsp;</b>
+          <textarea
+            id="description"
+            onFocus={(e) => {
+              e.target.style.overflowY = 'scroll';
+              e.target.style.height = '144pt';
+            }}
+            onBlur={(e) => {
+              e.target.style.overflowY = 'auto';
+              e.target.style.height = '48pt';
+            }}
+            contentEditable={false}
+            readOnly
+            defaultValue={description}
+          />
+        </li>
+      </ul>
+    </li>
+  );
+}
+
+JobApp.propTypes = {
+  company: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
